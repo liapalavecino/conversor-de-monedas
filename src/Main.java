6824751d42f1d992b15ec79f33ef323a3fa3606e
Lia <soledadlia@gmail.com>
2025-01-05 00:39:33 -0300

    Se crea la crea la clase APIResponse

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ import com.google.gson.Gson;
 public class Main {
     public static void main(String[] args) {
         Gson gson = new Gson();
-        String json = gson.toJson("¡Hola, Gson!");
+        String json = gson.toJson("Hola, soy un commit");
         System.out.println(json);
     }
 }
